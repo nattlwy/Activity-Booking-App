f3507dc178b872aec6899e44042414934223940d
IvanFung <fungc7@student.douglascollege.ca>
2023-11-01 12:53:48 -0700

    Main and Booking Page bare layout

---
+++ app/src/main/java/com/example/csis3175_grp6_proj/SportsBookingActivity2.java
@@ -0,0 +1,14 @@
+package com.example.csis3175_grp6_proj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SportsBookingActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sports_booking);
+    }
+}
