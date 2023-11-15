9771fbfb0cebddbcae01975a655b6947894908c7
Ivan Fung <fungc7@student.douglascollege.ca>
2023-11-14 18:13:16 -0800

    Login Session Management using shared preference in Main Activity

--- app/src/main/java/com/example/csis3175_grp6_proj/MainActivity.java
+++ app/src/main/java/com/example/csis3175_grp6_proj/MainActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
@@ -17,11 +19,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity  {
+    public static final String SHARED_PREFS = "shared_prefs";
+
+    SharedPreferences sharedPreferences;
+    String userId, password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startActivity(new Intent(MainActivity.this, HomeActivity.class));
+
+        sharedPreferences = getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
+        userId = sharedPreferences.getString("USER_ID", null);
+        // uncomment the line below can bypass the login for debug
+//        userId = "abc";
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent intent;
+        if (userId != null ) {
+            intent = new Intent(MainActivity.this, HomeActivity.class);
+        }
+        else {
+            intent = new Intent(MainActivity.this, LogIn.class);
+        }
+        startActivity(intent);
     }
 
 
