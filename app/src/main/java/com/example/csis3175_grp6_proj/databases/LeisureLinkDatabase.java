5339fef8d09b1a0f1f36a46047343e5f3cf90b6c
Wingki97 <78242525+Wingki97@users.noreply.github.com>
2023-11-17 12:35:21 -0800

    database content added

--- app/src/main/java/com/example/csis3175_grp6_proj/databases/LeisureLinkDatabase.java
+++ app/src/main/java/com/example/csis3175_grp6_proj/databases/LeisureLinkDatabase.java
@@ -1,6 +1,15 @@
 package com.example.csis3175_grp6_proj.databases;
 
+import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
+import com.example.csis3175_grp6_proj.interfaces.SportDao;
+import com.example.csis3175_grp6_proj.interfaces.UserDao;
+import com.example.csis3175_grp6_proj.models.Sport;
+import com.example.csis3175_grp6_proj.models.User;
+
+@Database(entities = {Sport.class, User.class}, version = 1, exportSchema = false)
 public abstract class LeisureLinkDatabase extends RoomDatabase {
+    public abstract SportDao sportDao();
+    public abstract UserDao userDao();
 }
