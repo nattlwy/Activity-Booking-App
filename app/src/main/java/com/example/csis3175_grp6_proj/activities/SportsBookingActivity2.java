9c7df62449aa6f535b8996364d7141818a13fffa
Ivan Fung <fungc7@student.douglascollege.ca>
2023-11-18 15:55:37 -0800

    Back button in sports booking activity and edit home sport icons size

--- app/src/main/java/com/example/csis3175_grp6_proj/activities/SportsBookingActivity2.java
+++ app/src/main/java/com/example/csis3175_grp6_proj/activities/SportsBookingActivity2.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.csis3175_grp6_proj.R;
 
@@ -21,6 +22,10 @@ public class SportsBookingActivity2 extends AppCompatActivity {
 
         Log.d("icyfung", sportName);
 
+        // title
+        TextView textViewTitle = findViewById(R.id.txtViewTitleBooking);
+        textViewTitle.setText(sportName + " Booking");
+
         /*
         1. pull venue and sessions data from database
         2. add the data to the two spinners
@@ -30,6 +35,14 @@ public class SportsBookingActivity2 extends AppCompatActivity {
             4.2 bundle data and start new activity
 
          */
+
+        // back button
+        Button backBtn = findViewById(R.id.btnBackSportBooking);
+        backBtn.setOnClickListener((View view) -> {
+            // back to home activity
+            finish();
+        });
+
 
         Button proceedBtn = findViewById(R.id.btnProceedBooking);
         proceedBtn.setOnClickListener((View view) -> {
