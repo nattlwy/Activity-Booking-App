1450c789ddd2d7aecb6c92027dd8de427dc723b2
Ivan Fung <fungc7@student.douglascollege.ca>
2023-11-23 21:44:09 -0800

    query venues and add to spinner in booking activity

--- app/src/main/java/com/example/csis3175_grp6_proj/activities/SportsBookingActivity2.java
+++ app/src/main/java/com/example/csis3175_grp6_proj/activities/SportsBookingActivity2.java
@@ -2,18 +2,36 @@ package com.example.csis3175_grp6_proj.activities;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.example.csis3175_grp6_proj.R;
+import com.example.csis3175_grp6_proj.databases.LeisureLinkDatabase;
+import com.example.csis3175_grp6_proj.models.Booking;
+import com.example.csis3175_grp6_proj.models.TimeSlot;
+import com.example.csis3175_grp6_proj.models.Venue;
+
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class SportsBookingActivity2 extends AppCompatActivity {
-
+    LeisureLinkDatabase lldb;
+    List<Venue> venueList;
+    List<TimeSlot> availableTimeslots;
+    Booking CurrBooking = new Booking();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +46,25 @@ public class SportsBookingActivity2 extends AppCompatActivity {
         TextView textViewTitle = findViewById(R.id.txtViewTitleBooking);
         textViewTitle.setText(sportName + " Booking");
 
+        // Init db
+        lldb = Room.databaseBuilder(getApplicationContext(), LeisureLinkDatabase.class,"leisurelink.db").build();
+
+        // Venue
+        LoadVenueFromDBToVenueSpinner();
+        Spinner venueSpinner = findViewById(R.id.spinnerVenueBooking);
+        venueSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Log.d("SportBooking", venueList.get(position).getVenueName());
+                CurrBooking.setVenueId(
+                        venueList.get(position).getVenueId()
+                );
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
         /*
         1. pull venue and sessions data from database
         2. add the data to the two spinners
@@ -45,8 +82,23 @@ public class SportsBookingActivity2 extends AppCompatActivity {
         calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int dayOfMonth) {
-                String date = dayOfMonth + "-" + (month + 1) + "-" + year;
-                Log.d("icyfung", date);
+                String dateStr = dayOfMonth + "/" + (month+1) + "/" + year;
+                Calendar selectedDate = Calendar.getInstance();
+                selectedDate.set(year, month, dayOfMonth);
+                int dayOfWeek = selectedDate.get(Calendar.DAY_OF_WEEK) - 1;
+                if (dayOfWeek == 0)
+                    dayOfWeek = 7;
+                Log.d("SportBooking", dateStr + " " + dayOfWeek);
+//                availableTimeslots = lldb.timeSlotDao().GetAvailableTimeSlotOfTheDay(dateStr, dayOfWeek);
+//                List<String> lst = new ArrayList<>();
+//                for (TimeSlot ts : availableTimeslots){
+//                    String timeslotStr = ts.getHour() + ":00 - " + (ts.getHour() + 2) + ":00" ;
+//                    Log.d("SportBooking", timeslotStr);
+//                    lst.add(timeslotStr);
+//                }
+//                Spinner spinnerTimeSlotBooking = findViewById(R.id.spinnerTimeSlotBooking);
+//                ArrayAdapter<String> arrAdapter = new ArrayAdapter<>(SportsBookingActivity2.this, android.R.layout.simple_spinner_item, lst);
+//                spinnerTimeSlotBooking.setAdapter(arrAdapter);
             }
         });
 
@@ -63,5 +115,26 @@ public class SportsBookingActivity2 extends AppCompatActivity {
 
         });
 
+    }
+
+    public void LoadVenueFromDBToVenueSpinner() {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                venueList = lldb.venueDao().GetAllVenues();
+                List<String> venueNameList = new ArrayList<>();
+                for (Venue ven: venueList) {
+                    venueNameList.add(ven.getVenueName());
+                }
+                Spinner venueSpinner = findViewById(R.id.spinnerVenueBooking);
+                // add venue to spinner
+                ArrayAdapter<String> arrAdapter = new ArrayAdapter<>(SportsBookingActivity2.this, android.R.layout.simple_spinner_item, venueNameList);
+                venueSpinner.setAdapter(arrAdapter);
+            }
+        });
+    }
+
+    public void LoadAvailableTimeSlotsFromDB() {
     }
 }
--- app/src/main/java/com/example/csis3175_grp6_proj/interfaces/TimeSlotDao.java
+++ app/src/main/java/com/example/csis3175_grp6_proj/interfaces/TimeSlotDao.java
@@ -24,4 +24,6 @@ public interface TimeSlotDao {
     @Query("SELECT * FROM timeslots")
     List<TimeSlot> GetAllTimeSlots();
 
+    @Query("SELECT * FROM TIMESLOTS WHERE timeslotid not in (SELECT timeslotid FROM BOOKINGS WHERE acivitydate = :date) AND dayofweek = :dayOfWeek")
+    List<TimeSlot> GetAvailableTimeSlotOfTheDay(String date, int dayOfWeek);
 }
